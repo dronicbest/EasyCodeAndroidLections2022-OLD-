5d62906a596cfcca843fc2fb7ea07132ac2925ad
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-06 16:59:27 +0300

    lecture 02. ImageView of Internet (Picasso)

--- HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
@@ -16,11 +16,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.ColorRes
 import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
     private companion object {
-        const val URL = "https://zavistnik.com/wp-content/uploads/2020/03/Android-kursy-zastavka.jpg"
+//        const val URL = "https://zavistnik.com/wp-content/uploads/2020/03/Android-kursy-zastavka.jpg"
+        const val URL = "https://images4.alphacoders.com/109/1095230.jpg"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,16 +33,23 @@ class MainActivity : AppCompatActivity() {
         image.setOnClickListener {
             image.setImageResource(R.mipmap.android_image)
         }
-        val netImage = NetImage(URL, object : ImageCallback {
-            override fun success(bitmap: Bitmap) {
-                image.setImageBitmap(bitmap)
-            }
 
-            override fun failed() {
-                Snackbar.make(image, "filed", Snackbar.LENGTH_SHORT).show()
-            }
-
-        })
-        netImage.start()
+        Picasso.get().load(URL).centerCrop()
+            .resize(720, 1280)
+            .placeholder(android.R.drawable.ic_media_pause)
+            .error(android.R.drawable.ic_dialog_alert)
+            .into(image)
+//
+//        val netImage = NetImage(URL, object : ImageCallback {
+//            override fun success(bitmap: Bitmap) = runOnUiThread {
+//                image.setImageBitmap(bitmap)
+//            }
+//
+//            override fun failed() = runOnUiThread {
+//                Snackbar.make(image, "filed", Snackbar.LENGTH_SHORT).show()
+//            }
+//
+//        })
+//        netImage.start()
     }
 }
