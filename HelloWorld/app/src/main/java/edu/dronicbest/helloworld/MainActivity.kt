7340624deb137277657dd034efaafcfead88fc6b
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-06 14:38:26 +0300

    lecture 01. HelloWorld.titleTextView

--- HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
@@ -1,11 +1,30 @@
 package edu.dronicbest.helloworld
 
+import android.content.res.Resources
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.ColorRes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        titleTextView.text = getText(R.string.hello_world)
+        titleTextView.setColor(R.color.black)
+        titleTextView.visibility = View.GONE
+    }
+
+    fun TextView.setColor(@ColorRes colorResId: Int, theme: Resources.Theme? = null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setTextColor(resources.getColor(colorResId, theme))
+        } else {
+            setTextColor(resources.getColor(colorResId))
+        }
     }
 }
