f6185cea20112618472a6b79884040828580009d
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-06 16:27:46 +0300

    lecture 02. ImageView of res

--- HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
@@ -11,6 +11,7 @@ import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.ColorRes
 import com.google.android.material.snackbar.Snackbar
@@ -20,46 +21,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val agreementTextView: TextView = findViewById(R.id.agreementTextView)
-        val fullText = getString(R.string.agreement_full_text)
-        val confidential = getString(R.string.confidential_info)
-        val policy = getString(R.string.privacy_policy)
-        val spannableString = SpannableString(fullText)
-
-        val confidentialClickable = MyClickableSpan {
-            Snackbar.make(it, "Go to link1", Snackbar.LENGTH_SHORT).show()
+        val image: ImageView = findViewById(R.id.iconImageView)
+        image.setOnClickListener {
+            image.setImageResource(R.mipmap.android_image)
         }
 
-        val policyClickable = MyClickableSpan {
-            Snackbar.make(it, "Go to link2", Snackbar.LENGTH_SHORT).show()
-        }
-
-        spannableString.setSpan(
-            confidentialClickable,
-            fullText.indexOf(confidential),
-            fullText.indexOf(confidential) + confidential.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        spannableString.setSpan(
-            policyClickable,
-            fullText.indexOf(policy),
-            fullText.indexOf(policy) + policy.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        agreementTextView.run {
-            text = spannableString
-            movementMethod = LinkMovementMethod.getInstance()
-            highlightColor = Color.TRANSPARENT
-        }
-    }
-
-    fun TextView.setColor(@ColorRes colorResId: Int, theme: Resources.Theme? = null) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            setTextColor(resources.getColor(colorResId, theme))
-        } else {
-            setTextColor(resources.getColor(colorResId))
-        }
     }
 }
