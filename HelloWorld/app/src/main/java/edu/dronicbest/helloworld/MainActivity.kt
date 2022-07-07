98b6bf07e02fdaed9e08b3b2c0383fad188ea1c6
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-07 16:28:08 +0300

    lecture 03. Submit enter Email and lambda listener

--- HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/edu/dronicbest/helloworld/MainActivity.kt
@@ -3,10 +3,13 @@ package edu.dronicbest.helloworld
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -35,8 +38,26 @@ class MainActivity : AppCompatActivity() {
 
         val textInputLayout = findViewById<TextInputLayout>(R.id.textInputLayout)
         textInputEditText = textInputLayout.editText as TextInputEditText
-
         textInputEditText.addTextChangedListener(textWatcher)
+        textInputEditText.listenerChanges { textInputLayout.isErrorEnabled = false }
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            if (Patterns.EMAIL_ADDRESS.matcher(textInputEditText.text.toString()).matches()) {
+                Snackbar.make(loginButton, "Go to postLogin", Snackbar.LENGTH_LONG)
+            } else {
+                textInputLayout.isErrorEnabled = true
+                textInputLayout.error = getString(R.string.invalid_email_address_msg)
+            }
+        }
+    }
+
+    fun TextInputEditText.listenerChanges(block: (test: String) -> Unit) {
+        addTextChangedListener(object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                block.invoke(s.toString())
+            }
+        })
     }
 
     fun TextInputEditText.setTextCorrectly(text: CharSequence) {
