56b67e1a107eab354c537d13ccab89e75a90cb79
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-11 10:28:47 +0300

    lecture 05. ViewModel - Timer

---
+++ HelloWorld/app/src/main/java/edu/dronicbest/helloworld/Model.kt
@@ -0,0 +1,19 @@
+package edu.dronicbest.helloworld
+
+import java.util.*
+
+class Model(private val textCallback: TextCallback) {
+    private var timer: Timer? = null
+    private var count = 0
+
+    fun start() {
+        timer?.cancel()
+        timer = Timer()
+        timer?.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                count++
+                textCallback.updateText(count.toString())
+            }
+        }, 1000, 1000)
+    }
+}
