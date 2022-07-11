7d1edad5ce9efb6590e9ba2a9c9fca6cdffc0fe7
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-11 16:47:24 +0300

    Main activity

--- JokeApp/app/src/main/java/edu/dronicbest/jokeapp/MainActivity.kt
+++ JokeApp/app/src/main/java/edu/dronicbest/jokeapp/MainActivity.kt
@@ -2,10 +2,43 @@ package edu.dronicbest.jokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = (application as JokeApp).viewModel
+        val button = findViewById<Button>(R.id.actionButton)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val textView = findViewById<TextView>(R.id.textView)
+        progressBar.visibility = View.INVISIBLE
+
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            viewModel.jetJoke()
+        }
+
+        viewModel.init(object : TextCallback {
+            override fun provideText(text: String) {
+                button.isEnabled = true
+                progressBar.visibility = View.INVISIBLE
+                textView.text = text
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        viewModel.clear()
+        super.onDestroy()
     }
 }
