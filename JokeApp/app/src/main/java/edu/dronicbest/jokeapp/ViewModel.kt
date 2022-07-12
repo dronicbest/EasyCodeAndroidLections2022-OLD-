43c46d9cf38f46a38bf7538fbcf1d00326d4ecaf
Andrey Tabatchikov <dronicbest@mail.ru>
2022-07-12 09:33:28 +0300

    ViewModel - remove generic

--- JokeApp/app/src/main/java/edu/dronicbest/jokeapp/ViewModel.kt
+++ JokeApp/app/src/main/java/edu/dronicbest/jokeapp/ViewModel.kt
@@ -1,12 +1,12 @@
 package edu.dronicbest.jokeapp
 
-class ViewModel(private val model: Model<Joke, JokeFailure>) {
+class ViewModel(private val model: Model) {
 
     private var callback: TextCallback? = null
 
     fun init(callback: TextCallback) {
         this.callback = callback
-        model.init(object : ResultCallback<Joke, JokeFailure> {
+        model.init(object : ResultCallback {
             override fun provideSuccess(data: Joke) = callback.provideText(data.getJokeUi())
             override fun provideError(error: JokeFailure) = callback.provideText(error.getMessage())
         })
@@ -26,13 +26,13 @@ interface TextCallback {
     fun provideText(text: String)
 }
 
-interface Model<S, E> {
+interface Model {
     fun getJoke()
-    fun init(callback: ResultCallback<S, E>)
+    fun init(callback: ResultCallback)
     fun clear()
 }
 
-interface ResultCallback<S, E> {
-    fun provideSuccess(data: S)
-    fun provideError(error: E)
+interface ResultCallback {
+    fun provideSuccess(data: Joke)
+    fun provideError(error: JokeFailure)
 }
